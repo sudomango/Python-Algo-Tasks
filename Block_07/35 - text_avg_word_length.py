4ba1ed3eab5ca207a90c4baba5499dde95c59a5d
sudomango <mark.saburov.work@gmail.com>
2023-10-23 16:20:49 +0000

    Добавлено решение для 35-ой задачи

---
+++ Block_07/35 - text_avg_word_length.py
@@ -0,0 +1,34 @@
+import re
+
+with open("../resources/some_plain_text.txt", "r", encoding = "UTF-8") as file:
+  content = file.read()
+
+# Очищаем слова в тексте от посторонних символов, типа знаков препинания (с помощью регулярного выражения).
+words = re.findall(r"\b\w+[\-‑]?\w*\b", content, flags = re.I | re.U | re.M)
+
+if (len(words) < 1):
+  print("Кажется, вы указали пустой файл. Мне нечего считать.")
+  exit()
+
+# Можно распечатать список слов, чтобы убедиться, что ничего лишнего в него не попало.
+# print(words, "\n", sep = "")
+
+total_letters = 0
+
+for word in words:
+  total_letters += len(word)
+
+avg_length = total_letters / len(words)
+
+def correct_text_output(number):
+  if (number in range(11, 15)):
+    return "символов"
+
+  if (number % 10 == 1):
+    return "символ"
+  elif (number % 10 in (2, 3, 4)):
+    return "символа"
+  else:
+    return "символов"
+
+print(f"Средняя длина слова в тексте составляет {avg_length:.2f} {correct_text_output(int(avg_length))}.")
